e542215e4f9a97c2cb955a005d966ae5f24627f8
김영은 <jessicak0828@gmail.com>
2023-12-29 17:05:57 +0900

    feat: seperate constants

---
+++ src/constants/index.ts
@@ -0,0 +1,53 @@
+export const TITLE = 'Front-End Developer';
+export const COLOR = {
+	WHITE: '#fff',
+	ORANGE: '#ed7c48',
+	IVORY: '#deded3',
+	BLUE: '#e5f0f3',
+	MINT: '#d5fcdf',
+	BROWN: '#1f1e1d',
+	BLACK: '#000',
+};
+export const FONT_SIZE = {
+	XS: '1.5rem',
+	S: '1.8rem',
+	M: '2rem',
+	L: '2.5rem',
+	XL: '3rem',
+	XXL: '4rem',
+	XXXL: '8rem',
+};
+
+export const FONT_WEIGHT = {
+	// THIN: 400,
+	// NORMAL: 500,
+	SEMI_BOLD: 700,
+	BOLD: 800,
+};
+
+export const SIZE = {
+	HEADER: '10vw',
+	FOOTER: '8vw',
+	INNER: '7vw',
+	HEADER_BTN: 30,
+	SKILL_IMG: 70,
+};
+
+export const SECTION = {
+	ABOUT: 'About',
+	ARCHIVE: 'Archive',
+	SKILLS: 'Skills',
+	PROJECTS: 'Projects',
+	CONTACT: 'Contact',
+};
+
+export const PROFILE = {
+	NAME: '🙍‍♀️이름',
+	AGE: '🎂나이',
+	ADDRESS: '🏚️주소',
+	PHONE_NUMBER: '📞전화번호',
+	EMAIL: '✉️이메일',
+};
+
+export const PROJECTS_DESCRIPTION = '프로젝트를 클릭하면 해당 프로젝트의 상세 페이지가 나옵니다.';
+export const FOOTER_TEXT = '© 2023. Kim Young En. All rights reserved.';
