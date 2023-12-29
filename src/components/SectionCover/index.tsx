4016a01dc81331bd90802233811856ef10114c26
김영은 <jessicak0828@gmail.com>
2023-12-29 17:12:05 +0900

    feat: create Section Cover

---
+++ src/components/SectionCover/index.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import Section from '@/components/common/Section';
+import { SIZE } from '@/constants';
+
+interface Props {
+	backgroundColor: string;
+}
+
+export default function SectionCover({ backgroundColor }: Props) {
+	return (
+		<Section backgroundColor={backgroundColor} marginTop={SIZE.HEADER} height="40rem" borderColor="transparent">
+			<Image src="/ocean.png" alt="" fill style={{ objectFit: 'cover' }} />
+		</Section>
+	);
+}
