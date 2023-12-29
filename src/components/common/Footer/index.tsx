ffa5c62d09a359474d564099bd8d4e90c156c16f
김영은 <jessicak0828@gmail.com>
2023-12-29 17:08:14 +0900

    feat: create Component Footer

---
+++ src/components/common/Footer/index.tsx
@@ -0,0 +1,9 @@
+import { PropsWithChildren } from 'react';
+import { COLOR } from '@/constants';
+interface Props extends PropsWithChildren {
+	backgroundColor: string;
+}
+
+export default function Footer({ children, backgroundColor }: Props) {
+	return <footer style={{ backgroundColor: backgroundColor, color: COLOR.WHITE }}>{children}</footer>;
+}
