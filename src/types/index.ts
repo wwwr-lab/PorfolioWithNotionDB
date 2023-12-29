acfa9b0ffefa4997b2b866801b62c26bc37ee95f
김영은 <jessicak0828@gmail.com>
2023-12-29 17:13:06 +0900

    feat: seperate types

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type SectionTitle = string;
+
+export type ArchiveData = {
+	name: string;
+	logos: string[];
+	images: string[];
+	link: string;
+	description: string;
+};
+
+export type ProjectData = {
+	projectId: string;
+	projectTitle: string;
+};
+export type ProjectDetailData = {
+	title: string;
+	link: string;
+	image: string;
+	term: string;
+	people: string;
+	summary: string;
+};
